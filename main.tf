fb0c249f287420287826a385ddaa046cf7ec5a3b
Sergey Vasilenko <svasilenko@mirantis.com>
2019-09-20 13:57:09 +0300

    with one subnet, ssh worked

--- main.tf
+++ main.tf
@@ -22,13 +22,12 @@ resource "openstack_networking_subnet_v2" "public" {
     network_id = "${openstack_networking_network_v2.public.id}"
     cidr = "192.168.1.0/24"
     ip_version = 4
-    # dns_nameservers = ["8.8.8.8","8.8.4.4"]
+    // dns_nameservers = ["8.8.8.8","8.8.4.4"]
 }
 
 resource "openstack_networking_router_v2" "public" {
     name = "${var.pref}_public"
     admin_state_up = "true"
-    # external_gateway = "true"
     external_network_id = "${data.openstack_networking_network_v2.external.id}"
 }
 
@@ -47,15 +46,78 @@ resource "openstack_networking_subnet_v2" "internal" {
     network_id = "${openstack_networking_network_v2.internal.id}"
     cidr = "10.10.10.0/24"
     ip_version = 4
-    # dns_nameservers = ["8.8.8.8","8.8.4.4"]
+    // dns_nameservers = ["8.8.8.8","8.8.4.4"]
 }
 #------------------------------------------------------------------------------
+resource "openstack_networking_secgroup_v2" "secgroup" {
+  name                 = "${var.pref}_secgroup"
+  delete_default_rules = true
+}
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_v4_egress" {
+  direction         = "egress"
+  ethertype         = "IPv4"
+  security_group_id = "${openstack_networking_secgroup_v2.secgroup.id}"
+  depends_on        = ["openstack_networking_secgroup_v2.secgroup"]
+}
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_v4_egress_tcp" {
+  direction         = "egress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 1
+  port_range_max    = 65535
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.secgroup.id}"
+  depends_on        = ["openstack_networking_secgroup_v2.secgroup"]
+}
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_v4_egress_udp" {
+  direction         = "egress"
+  ethertype         = "IPv4"
+  protocol          = "udp"
+  port_range_min    = 1
+  port_range_max    = 65535
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.secgroup.id}"
+  depends_on        = ["openstack_networking_secgroup_v2.secgroup"]
+}
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_v4_egress_icmp" {
+  direction         = "egress"
+  ethertype         = "IPv4"
+  protocol          = "icmp"
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.secgroup.id}"
+  depends_on        = ["openstack_networking_secgroup_v2.secgroup"]
+}
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_v4_ingress_tcp" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 1
+  port_range_max    = 65535
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.secgroup.id}"
+  depends_on        = ["openstack_networking_secgroup_v2.secgroup"]
+}
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_v4_ingress_udp" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "udp"
+  port_range_min    = 1
+  port_range_max    = 65535
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.secgroup.id}"
+  depends_on        = ["openstack_networking_secgroup_v2.secgroup"]
+}
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_v4_ingress_icmp" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "icmp"
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.secgroup.id}"
+  depends_on        = ["openstack_networking_secgroup_v2.secgroup"]
+}
 #------------------------------------------------------------------------------
 data "openstack_images_image_v2" "ubuntu" {
   name = "${var.cp_instance_image_name}"
-}
-data "openstack_compute_flavor_v2" "cp_instance_flavor" {
-  name = "${var.cp_instance_flavor_name}"
 }
 
 resource "openstack_networking_floatingip_v2" "cp_instance_floating_ip" {
@@ -66,18 +128,31 @@ resource "openstack_compute_floatingip_associate_v2" "cp_instance_floating_ip" {
   instance_id = "${openstack_compute_instance_v2.cp_instance.id}"
 }
 
+# because it prohibited by policy
+# resource "openstack_compute_flavor_v2" "cp_instance_flavor" {
+#   name  = "${var.pref}_cp_instance_flavor"
+#   ram   = "8096"
+#   vcpus = "4"
+#   disk  = "10"
+# }
+data "openstack_compute_flavor_v2" "cp_instance_flavor" {
+  name = "${var.cp_instance_flavor_name}"
+}
+
 resource "openstack_compute_instance_v2" "cp_instance" {
   name      = "${var.pref}_cp_instance"
   image_id  = "${data.openstack_images_image_v2.ubuntu.id}"
+# flavor_id = "${openstack_compute_flavor_v2.cp_instance_flavor.id}"  // if flavor creation allowed by policy
   flavor_id = "${data.openstack_compute_flavor_v2.cp_instance_flavor.id}"
   key_pair  = "${openstack_compute_keypair_v2.auth_kp.id}"
+  security_groups = ["${openstack_networking_secgroup_v2.secgroup.id}"]
 
-  network {
+  network {  // network number #0
     uuid = "${openstack_networking_network_v2.public.id}"
   }
-  network {
-    uuid = "${openstack_networking_network_v2.internal.id}"
-  }
+#   network {  // network number #1
+#     uuid = "${openstack_networking_network_v2.internal.id}"
+#   }
 }
 
 ###
--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,7 @@
+output "cp_instance_floating_ip_addr" {
+  value = openstack_compute_floatingip_associate_v2.cp_instance_floating_ip.floating_ip
+}
 
+# output "cp_instance_internal_ip_addr" {
+#   value = openstack_compute_instance_v2.cp_instance.network.1.fixed_ip_v4
+# }
